1cb37797e4b5dd3eff7a50689803755fd1f41e65
Nathan Kittichaikoonkij <Natsg90@gmail.com>
2024-12-14 17:32:28 +0700

    Rename files

--- TSP/tsp.py
+++ TSP/tsp.py
@@ -23,9 +23,15 @@ class TSP(BaseQUBO):
     def qubo(self, solver, penalty = 10):
         super().qubo(solver, penalty)
         if solver == "Qiskit":
-            pass
+            qp = tsp_quad(self.weight_matrix)
+            qp2qubo = QuadraticProgramToQubo(penalty=penalty)
+            qubo = qp2qubo.convert(qp)
+            qubitOp, offset = qubo.to_ising()
+            return {"model": qubitOp, "offset":  offset}
+        else:
+            return {"model": None}
     
-    def interpret(self, result, solver = "Gurobi", verbose=False):
+    def interpret(self, result, solver, verbose=False):
         return super().interpret(result, solver, verbose)
     
     def brute_force(self):
@@ -66,4 +72,56 @@ def generate_tsp(n: int, seed: int, draw: bool, max_distance=100):
     return graph, sym
 
 
+def tsp_quad(weights: np.ndarray) -> QuadraticProgram:
+    """
+    Create a Quadratic Program formulation for the Traveling Salesman Problem (TSP).
 
+    The function takes a matrix of pairwise city distances and encodes the TSP 
+    problem as a Quadratic Program (QP). The QP formulation aims to minimize 
+    the total distance traveled by visiting each city exactly once and returning
+    to the starting city.
+
+    Parameters
+    ----------
+    weights : np.ndarray
+        A 2D square matrix where `weights[i, j]` represents the distance between 
+        city `i` and city `j`. The matrix must have shape `(n, n)`, where `n` is 
+        the number of cities.
+
+    Returns
+    -------
+    qp : QuadraticProgram
+        A Quadratic Program representing the TSP problem. The objective is to minimize 
+        the total distance of the tour, and the constraints ensure that each city is 
+        visited exactly once and left exactly once.
+"""
+    qp = QuadraticProgram()
+
+    size = weights.shape[0]
+
+    cities = {}  # dictionary to store the city and order
+    for i in range(size):
+        for p in range(size):
+            cities[f'x_{i}_{p}'] = qp.binary_var(f'x_{i}_{p}')
+
+    # Objective function
+    quadratic_matrix = {}
+    for i in range(size):
+        for j in range(size):
+            if i != j:
+                for p in range(size):
+                    quadratic_matrix[(f'x_{i}_{p}', f'x_{j}_{(p+1) % size}')] = weights[i, j]
+
+    qp.minimize(quadratic=quadratic_matrix)
+
+    # Constraint 1: each city is visited exactly once
+    for i in range(size):
+        qp.linear_constraint(
+            linear={f'x_{i}_{p}': 1 for p in range(size)}, sense='==', rhs=1)
+
+    # Constraint 2: each city is left exactly once
+    for p in range(size):
+        qp.linear_constraint(
+            linear={f'x_{i}_{p}': 1 for i in range(size)}, sense='==', rhs=1)
+
+    return qp
